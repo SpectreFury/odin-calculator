8481db18ab534da8c46e03f8d943efbc13a3ceb6
Ayush Soni <soni.ayush.2212@gmail.com>
2022-08-21 02:26:44 +0530

    Add keyboard support

--- script.js
+++ script.js
@@ -11,6 +11,81 @@ let secondValue = 0;
 let result = 0;
 let clickedIsEquals = false;
 
+// Keyboard Handler
+const numberKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const operationKeys = ["+", "-", "/", "*"];
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Backspace") {
+    if (!screen.textContent) {
+      console.log("Empty sadly");
+    } else {
+      screen.textContent = screen.textContent.slice(
+        0,
+        screen.textContent.length - 1
+      );
+    }
+  }
+  operationKeys.forEach((key) => {
+    if (e.key === key) {
+      clickedIsEquals = false;
+      if (key === "+") {
+        if (result) {
+          firstValue = result;
+          chosenOperator = "+";
+          screen.textContent = "";
+        } else {
+          firstValue = +screen.textContent;
+          chosenOperator = "+";
+          screen.textContent = "";
+        }
+      } else if (key === "-") {
+        if (result) {
+          firstValue = result;
+          chosenOperator = "-";
+          screen.textContent = "";
+        } else {
+          firstValue = +screen.textContent;
+          chosenOperator = "-";
+          screen.textContent = "";
+        }
+      } else if ((key = "*")) {
+        if (result) {
+          firstValue = result;
+          chosenOperator = "*";
+          screen.textContent = "";
+        } else {
+          firstValue = +screen.textContent;
+          chosenOperator = "*";
+          screen.textContent = "";
+        }
+      } else if (key === "/") {
+        if (result) {
+          firstValue = result;
+          chosenOperator = "/";
+          screen.textContent = "";
+        } else {
+          firstValue = +screen.textContent;
+          chosenOperator = "/";
+          screen.textContent = "";
+        }
+      } else {
+        return;
+      }
+    }
+  });
+
+  numberKeys.forEach((key) => {
+    if (e.key === key) {
+      if (clickedIsEquals) {
+        allClear();
+        clickedIsEquals = false;
+      }
+      screen.textContent += key;
+    }
+  });
+});
+
 // Adds numbers to the display
 numberBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -28,7 +103,10 @@ clearBtn.addEventListener("click", () => {
   if (!screen.textContent) {
     console.log("Empty sadly");
   } else {
-    screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1);
+    screen.textContent = screen.textContent.slice(
+      0,
+      screen.textContent.length - 1
+    );
   }
 });
 
